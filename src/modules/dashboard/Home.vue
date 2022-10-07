5a6bf39c400e48235608ec98202220a6fd72c02d
Shine Kyaw Kyaw Aung <shinekyawkyawaung@onenex.co>
2022-10-07 09:58:41 +0630

    remove htmlcanvas

--- src/modules/dashboard/Home.vue
+++ src/modules/dashboard/Home.vue
@@ -1,44 +1,13 @@
 <template>
   <div>
-    <div ref="printcontent">Please print me ):</div>
-    <button @click="printMe">Print Me</button>
+    <h1>Home</h1>
   </div>
 </template>
 <script>
-import { defineComponent, ref } from "vue"
-import html2canvas from "html2canvas"
+import { defineComponent } from "vue"
 export default defineComponent({
   name: "Dashboard",
-  setup() {
-    const printcontent = ref()
-
-    const printMe = async () => {
-      console.log("printing..")
-      const el = printcontent.value
-
-      const options = {
-        type: "dataURL"
-      }
-      const printCanvas = await html2canvas(el, options)
-
-      const link = document.createElement("a")
-      link.setAttribute("download", "download.png")
-      link.setAttribute(
-        "href",
-        printCanvas
-          .toDataURL("image/png")
-          .replace("image/png", "image/octet-stream")
-      )
-      link.click()
-
-      console.log("done")
-    }
-
-    return {
-      printcontent,
-      printMe
-    }
-  }
+  setup() {}
 })
 </script>
 <style lang=""></style>
